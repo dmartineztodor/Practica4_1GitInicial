1a8475643ac64a5a957c79165bcdd54926faecb4
dmartinez <2889973@alu.murciaeduca.es>
2025-02-04 13:11:13 -0500

    Añadir clase Usuario e implementar en Principal.java el metodo crearUsuarios

--- Principal.java
+++ Principal.java
@@ -1,15 +1,44 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Ingresa la cantidad de usuarios que deseas crear: ");
+        int cantidad = teclado.nextInt();
+        teclado.nextLine(); // Limpiar Buffer
+        Usuario[] usuarios = crearUsuarios(cantidad);
+
         System.out.println("Hola mundo.");
         mostrarFechaHora();
+        for (Usuario usuario : usuarios) {
+            System.out.println(usuario.nombre + " " + usuario.apellidos + " " + usuario.email);
+        }
+
+        teclado.close();
     }
 
     public static void mostrarFechaHora() {
         LocalDateTime ahora = LocalDateTime.now();
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         System.out.println("Fecha y hora actual: " + ahora.format(formato));
+    }
+
+    public static Usuario[] crearUsuarios(int cantidad) {
+        Scanner teclado = new Scanner(System.in);
+        Usuario[] usuarios = new Usuario[cantidad];
+
+        for (int i = 0; i < cantidad; i++) {
+            usuarios[i] = new Usuario();
+            System.out.println("Nombre: ");
+            usuarios[i].nombre = teclado.nextLine();
+            System.out.println("Apellidos: ");
+            usuarios[i].apellidos = teclado.nextLine();
+            System.out.println("Email: ");
+            usuarios[i].email = teclado.nextLine();
+        }
+        teclado.close();
+        return usuarios;
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-# Practica4_1GitInicial
+# Práctica4_1GitInicial
 Práctica 4.1 Git Inicial: Entornos de desarrollo 2024-2025
 ## Descripción
-Los cambios implementados son añadir una función que muestra la fecha y la hora actual.
+Este programa escrito en el lenguaje **Java** sirve para imprimir un 'Hola Mundo' por consola, para mostrar la fecha y hora actual y para añadir nuevos usuarios.
+
+## Estructura
+- `Principal.java`: Clase principal del programa.
+- `Usuario.java`: Clase para un usuario con sus atributos.
+- `.gitignore`: Archivo para ignorar los .class innecesarios.
+- `README.md`: Documentación de la práctica.
+
+## Ramas
+- `main`: Rama principal del proyecto.
+- `ractica4_1`: En esta rama se introdujo la clase para mostrar la **fecha** y **hora.**
+- `Practica4_1B`: En esta ramo se introdujo la clase **Usuario.**
+
+### Autor
+Práctica creada por Daniel Martínez Todor.
